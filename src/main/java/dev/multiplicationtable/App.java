7c6a110d023fa52f6baf3b3a448f2948c83dc6a4
duran-ni <duran_ni_7@hotmail.com>
2026-07-22 11:15:31 +0200

    feat: add App entry point to print multiplication table

---
+++ src/main/java/dev/multiplicationtable/App.java
@@ -0,0 +1,9 @@
+package dev.multiplicationtable;
+
+public class App {
+
+    public static void main(String[] args) {
+        MultiplicationTable table = new MultiplicationTable();
+        table.print(7);
+    }
+}
